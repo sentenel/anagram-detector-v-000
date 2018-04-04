e042ec8ee021617f28247407fda80168b3356cbe
Seth Wax <sethlwax@gmail.com>
2018-04-04 15:24:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def self.anagram?(word_1, word_2)
+    word_1.split('').sort == word_2.split('').sort
+  end
+
+  def match(words)
+    words.select{|w| Anagram.anagram?(w, word)}
+  end
+end
